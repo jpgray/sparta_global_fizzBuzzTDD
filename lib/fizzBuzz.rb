dd3b2184f5fe9b95920fe0c42e9b3875fe94c092
Jon Gray <jgray@spartaglobal.com>
2018-11-20 15:58:08 +0000

    v1.4 refactored for testing

--- lib/fizzBuzz.rb
+++ lib/fizzBuzz.rb
@@ -1,27 +1,55 @@
 class FizzBuzz
-  def fizzBuzz (startNum, endNum)
+
+  def fizz (num)
+    if num % 3 == 0
+      true
+    else
+      false
+    end
+  end
+
+  def buzz (num)
+    if num % 5 == 0
+      true
+    else
+      false
+    end
+  end
+
+  def fizzBuzz (num)
+    if num % 3 == 0 and num % 5 == 0
+      true
+    else
+      false
+    end
+  end
+
+  def fullFizzBuzz (startNum, endNum)
+    fbArray = []
     i = startNum
     while i<=endNum
-      if (i % 3 == 0 and i % 5 == 0)
-        print "fizzBuzz "
+      if fizzBuzz(i) == true
+        # print "fizzBuzz "
+        fbArray.push("fizzBuzz ")
 
+      elsif buzz(i) == true
+        # print "buzz "
+        fbArray.push("buzz ")
 
-      elsif (i % 5 == 0)
-        print "buzz "
-
-
-      elsif (i % 3 == 0)
-        print "fizz "
-
+      elsif fizz(i) == true
+        # print "fizz "
+        fbArray.push("fizz ")
 
       else
-        print "#{i} "
+        # print "#{i} "
+        fbArray.push(i)
       end
 
       i += 1
     end
+    print fbArray
   end
 end
 
 play = FizzBuzz.new
-play.fizzBuzz(15, 32)
+play.fullFizzBuzz(15,32)
